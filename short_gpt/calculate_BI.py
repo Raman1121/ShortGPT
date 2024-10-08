64237b83eade888e3a4d60b0063a3c5d4016f187
r50040918 <raman.dutt@huawei.com>
2024-10-08 10:51:44 +0100

    Some changes for model healing

--- short_gpt/calculate_BI.py
+++ short_gpt/calculate_BI.py
@@ -8,7 +8,7 @@ from peft import (
     LoraConfig,
     TaskType,
 )
-from transformers import default_data_collator, Trainer, TrainingArguments
+from transformers import default_data_collator, Trainer, TrainingArguments, BitsAndBytesConfig, DataCollatorForSeq2Seq
 import torch.nn as nn
 from short_hf import ShortHFModel
 from transformers import AutoModelForCausalLM, BitsAndBytesConfig
@@ -16,7 +16,7 @@ import argparse
 import shutil
 import json
 import os
-from data import get_preprocessed_samsum, create_peft_config, get_alpaca_small
+from data import get_preprocessed_samsum, create_peft_config, get_alpaca_small, get_medical_dataset
 from trl import SFTTrainer
 from peft import PeftConfig, get_peft_model, prepare_model_for_kbit_training
 
@@ -44,88 +44,110 @@ def main(args):
     MODEL_SAVEPATH = os.path.join(ROOT_SAVEPATH, "Pruned_Models", model_name)
     original_model_path = MODEL_PATH_DICT[args.model]
 
-    data = load_dataset(args.dataset, split="train")  # authors sample 10,000 texts to compute block influences
+    # data = load_dataset(args.dataset, split="train")  # authors sample 10,000 texts to compute block influences
 
-    dataloader = DataLoader(
-        data,
-        batch_size=args.batch_size,
-        shuffle=True,
-    )
+    # dataloader = DataLoader(
+    #     data,
+    #     batch_size=args.batch_size,
+    #     shuffle=True,
+    # )
 
-    MAX_SEQ_LEN = 1024
-    short_model = ShortHFModel(
-        model_name=MODEL_PATH_DICT[args.model],
-        layers_path="model.layers",
-        n_prune_layers=args.num_layers_to_prune,
-    )
+    # MAX_SEQ_LEN = 1024
+    # short_model = ShortHFModel(
+    #     model_name=MODEL_PATH_DICT[args.model],
+    #     layers_path="model.layers",
+    #     n_prune_layers=args.num_layers_to_prune,
+    # )
 
-    # MAX_SEQ_LEN = short_model.model.config.max_position_embeddings
+    # # MAX_SEQ_LEN = short_model.model.config.max_position_embeddings
 
-    original_model = AutoModelForCausalLM.from_pretrained(MODEL_PATH_DICT[args.model], device_map='cpu')    # Original model is on CPU because we need it only to calculate the numnber of pruned parameters
-    tokenizer = short_model.tokenizer
+    # original_model = AutoModelForCausalLM.from_pretrained(MODEL_PATH_DICT[args.model], device_map='cpu')    # Original model is on CPU because we need it only to calculate the numnber of pruned parameters
+    # tokenizer = short_model.tokenizer
 
-    for i, batch in enumerate(tqdm(dataloader)):
-        prompts = batch['text']
+    # for i, batch in enumerate(tqdm(dataloader)):
+    #     prompts = batch['text']
 
-        short_model.eval_importance(
-            prompts=prompts,
-            max_seq_len=MAX_SEQ_LEN,
-            stride=256,
-            max_gen_len=0,
-            angular=args.angular,
-        )
+    #     short_model.eval_importance(
+    #         prompts=prompts,
+    #         max_seq_len=MAX_SEQ_LEN,
+    #         stride=256,
+    #         max_gen_len=0,
+    #         angular=args.angular,
+    #     )
 
-    print("############ IMPORTANCES")
-    print(short_model.importances)
-    print("############ REMOVE LAYERS")
-    layers_to_remove = short_model.remove_layers()
-    print(layers_to_remove)
+    # print("############ IMPORTANCES")
+    # print(short_model.importances)
+    # print("############ REMOVE LAYERS")
+    # layers_to_remove = short_model.remove_layers()
+    # print(layers_to_remove)
 
-    len(original_model.model.layers), len(short_model.model.model.layers)
+    # len(original_model.model.layers), len(short_model.model.model.layers)
 
-    # CALCULATING THE NUMBER OF PARAMETERS
-    for _layer_idx in layers_to_remove:
-        PARAMETER_BUDGET += count_parameters(original_model.model.layers[_layer_idx])
+    # # CALCULATING THE NUMBER OF PARAMETERS
+    # for _layer_idx in layers_to_remove:
+    #     PARAMETER_BUDGET += count_parameters(original_model.model.layers[_layer_idx])
 
-    print("##### PARAMETER BUDGET: ", PARAMETER_BUDGET)
+    # print("##### PARAMETER BUDGET: ", PARAMETER_BUDGET)
 
-    """
-    Saving the pruned model
-    """
-    short_model.model.save_pretrained(MODEL_SAVEPATH)
+    # """
+    # Saving the pruned model
+    # """
+    # short_model.model.save_pretrained(MODEL_SAVEPATH)
 
-    # Copying tokenizer and other configs from the original Model for Completion
-    print("!!! Copying tokenizer and other configs from the original Model for Completion")
+    # # Copying tokenizer and other configs from the original Model for Completion
+    # print("!!! Copying tokenizer and other configs from the original Model for Completion")
     
-    shutil.copyfile(original_model_path+"/tokenizer.json", os.path.join(MODEL_SAVEPATH, "tokenizer.json"))
-    shutil.copyfile(original_model_path+"/tokenizer_config.json", os.path.join(MODEL_SAVEPATH, "tokenizer_config.json"))
-    shutil.copyfile(original_model_path+"/special_tokens_map.json", os.path.join(MODEL_SAVEPATH, "special_tokens_map.json"))
+    # shutil.copyfile(original_model_path+"/tokenizer.json", os.path.join(MODEL_SAVEPATH, "tokenizer.json"))
+    # shutil.copyfile(original_model_path+"/tokenizer_config.json", os.path.join(MODEL_SAVEPATH, "tokenizer_config.json"))
+    # shutil.copyfile(original_model_path+"/special_tokens_map.json", os.path.join(MODEL_SAVEPATH, "special_tokens_map.json"))
     
-    print("!!! Editing the new config file to reflect the number of layers")
-    # Read json file
-    data = json.load(open(MODEL_PATH_DICT[args.model] + "/config.json", "r"))
-    data['num_hidden_layers'] = data['num_hidden_layers'] - args.num_layers_to_prune
+    # print("!!! Editing the new config file to reflect the number of layers")
+    # # Read json file
+    # data = json.load(open(MODEL_PATH_DICT[args.model] + "/config.json", "r"))
+    # data['num_hidden_layers'] = data['num_hidden_layers'] - args.num_layers_to_prune
 
-    print("!!! New Number of Layers: ", data['num_hidden_layers'])
+    # print("!!! New Number of Layers: ", data['num_hidden_layers'])
 
-    # Save the new config file
-    print("!!! Saving the new config file")
-    json.dump(data, open(os.path.join(MODEL_SAVEPATH, "config.json"), "w"))
-    print("!!! Finished")
+    # # Save the new config file
+    # print("!!! Saving the new config file")
+    # json.dump(data, open(os.path.join(MODEL_SAVEPATH, "config.json"), "w"))
+    # print("!!! Finished")
 
 
     if(args.do_model_healing):
-        print("Performing model healing by finetuning on Alpaca dataset")
-        print("Loading model {}".format(MODEL_SAVEPATH))
+        print("Performing model healing by finetuning")
+        print("### Learning Rate: ", args.lr)
         
+        if args.pruned_model_ckpt is not None:
+            MODEL_SAVEPATH = args.pruned_model_ckpt
+
+        print("Loading model {}".format(MODEL_SAVEPATH))
+
+        if(args.use_bnb):
+            torch_dtype = torch.float16
+            bnb_config = BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_compute_dtype=torch_dtype,
+                bnb_4bit_use_double_quant=True,
+            )
+        else:
+            bnb_config = None
+
         short_model = ShortHFModel(
             model_name=MODEL_SAVEPATH,
             layers_path="model.layers",
             n_prune_layers=args.num_layers_to_prune,
+            bnb_config=bnb_config,
         )
 
         tokenizer = short_model.tokenizer
         peft_model, lora_config = create_peft_config(short_model.model, args)
+
+        if(args.use_bnb):
+            peft_model = prepare_model_for_kbit_training(peft_model)
+
+        peft_model.print_trainable_parameters()
 
         output_dir = os.path.join(MODEL_SAVEPATH, "logs")
 
@@ -136,13 +158,17 @@ def main(args):
             'gradient_checkpointing': False,
         }
 
-        dataset = get_alpaca_small(tokenizer) # Alpaca Dataset
+        # dataset = get_alpaca_small(tokenizer) # Alpaca Dataset
+
+        tokenized_train_dataset, tokenized_val_dataset = get_medical_dataset(tokenizer)
 
         training_args = TrainingArguments(
             output_dir=output_dir,
             overwrite_output_dir=True,
             # logging strategies
             logging_strategy="steps",
+            warmup_steps=args.warmup_steps,
+            lr_scheduler_type="cosine",
             per_device_train_batch_size=args.healing_batch_size,
             gradient_accumulation_steps=args.grad_acc_steps,
             logging_steps=10,
@@ -151,16 +177,42 @@ def main(args):
             **{k:v for k,v in config.items() if k != 'lora_config'}
         )
 
-        
-
         # Create Trainer instance
-        trainer = SFTTrainer(
+        # trainer = SFTTrainer(
+        #     model=peft_model,
+        #     args=training_args,
+        #     train_dataset = dataset,
+        #     dataset_text_field = "text",
+        #     # fp16=True,
+        #     callbacks=[],
+        # )
+
+        # dataset_train = load_dataset("timdettmers/openassistant-guanaco", split="train")
+        # dataset_test = load_dataset("timdettmers/openassistant-guanaco", split="test")
+
+        # tokenizer.add_special_tokens({'pad_token': '<PAD>'})
+
+        # trainer = SFTTrainer(
+        #     model=peft_model,
+        #     args=training_args,
+        #     train_dataset=dataset_train,
+        #     eval_dataset=dataset_test,
+        #     dataset_text_field="text",
+        #     max_seq_length=512,
+        #     tokenizer=tokenizer,
+        #     packing=True,
+        # )
+
+        trainer = Trainer(
             model=peft_model,
+            train_dataset=tokenized_train_dataset,
+            eval_dataset=tokenized_val_dataset,
             args=training_args,
-            train_dataset = dataset,
-            dataset_text_field = "text",
-            callbacks=[],
+            data_collator=DataCollatorForSeq2Seq(
+                tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+            ),
         )
+
 
         # Start training
         trainer.train()
@@ -177,9 +229,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=8)
-    parser.add_argument("--healing_batch_size", type=int, default=2)
+    parser.add_argument("--healing_batch_size", type=int, default=1)
     parser.add_argument("--dataset", type=str, default="arcee-ai/sec-data-mini")
     parser.add_argument("--model", type=str, default="llama3")
+    parser.add_argument("--pruned_model_ckpt", type=str, default=None)
     parser.add_argument("--num_layers_to_prune", type=int, default=5)
 
     parser.add_argument("--do_model_healing", action="store_true")
@@ -189,6 +242,8 @@ if __name__ == "__main__":
     parser.add_argument("--alpha", type=int, default=32)
     parser.add_argument("--grad_acc_steps", type=int, default=8)
     parser.add_argument("--angular", action="store_true")
+    parser.add_argument("--use_bnb", action="store_true")
+    parser.add_argument("--warmup_steps", type=int, default=100)
 
     args = parser.parse_args()
 
